81bbb7d4d650065e2a035d4992bbfe95911744c7
krivse <valones75@gmail.com>
2022-11-15 04:21:06 +0300

    on the review: deploy remte server v1.3

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -14,7 +14,8 @@ class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.author == request.user or request.user.is_superuser == request.user
+        return (obj.author == request.user or
+                request.user.is_superuser == request.user)
 
 
 class IsCurrentUserOrAdminOrReadOnly(permissions.BasePermission):
